b7f57fcd21066b32b94c5881ee53835c461c8ad3
shohan <shohan889@yahoo.com>
2022-09-23 23:31:50 +0600

    Companies obj added

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const server = require('node:http')
 
 
+let companies = [
+    {
+      name: "company A",
+      logo: "https://placekitten.com/200/300",
+      Specialties: ["Excavation", "Plumbing", "Electrica"],
+    },
+    {
+      name: "company B",
+      logo: "https://placekitten.com/200/300",
+      Specialties: ["Excavation"],
+    },
+    {
+      name: "company C",
+      logo: "https://placekitten.com/200/300",
+      Specialties: ["Excavation", "Plumbing"],
+    },
+  ];
 
 server.createServer((req,res)=>{
 
     if(req.url=='/'){
-        res.end("Home page")
+
+        console.log(companies);
+        res.end(companies[0].name)
     }
     if(req.url=="/article"){
         res.end(`<h2>Artcicle page12</h2>`)
     }
-    console.log(req)
+    // console.log(req)
     // console.log(res);
 
 }).listen(3000)
